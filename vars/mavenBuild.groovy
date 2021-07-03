c6be6ffaf1e2ef7ecd21a7fc537514adab3ef607
jyotichaudhury89 <jyoti.chaudhury@capgemini.com>
2021-07-03 19:02:39 +0530

    debugging

--- vars/mavenBuild.groovy
+++ vars/mavenBuild.groovy
@@ -1,6 +1,7 @@
 import com.sap.piper.BuildTool
 import com.sap.piper.DownloadCacheUtils
 import groovy.transform.Field
+import groovy.io.FileType
 
 import static com.sap.piper.Prerequisites.checkScript
 
@@ -11,6 +12,16 @@ void call(Map parameters = [:]) {
     List credentials = [[type: 'token', id: 'altDeploymentRepositoryPasswordId', env: ['PIPER_altDeploymentRepositoryPassword']]]
     final script = checkScript(this, parameters) ?: this
     parameters = DownloadCacheUtils.injectDownloadCacheInParameters(script, parameters, BuildTool.MAVEN)
+	println("DEBUGGING ---- START ----")
+	def list = []
+
+	def dir = new File("./")
+	dir.eachFileRecurse (FileType.FILES) { file ->
+	  list << file
+	}
+	list.each {
+	  println it.path
+	}
+	println("DEBUGGING ---- END ----")
     piperExecuteBin(parameters, STEP_NAME, METADATA_FILE, credentials)
-	println("DEBUGGING: Checking if this line is reached after the piperExecuteBin method!")
 }
