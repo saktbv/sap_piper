cf5f78f648ffaebab21e469ed4960401441292fb
jyotichaudhury89 <jyoti.chaudhury@capgemini.com>
2021-09-02 20:03:10 +0530

    pepeline changes

--- vars/piperPipeline.groovy
+++ vars/piperPipeline.groovy
@@ -92,6 +92,7 @@ void call(parameters) {
 					unstash(name: 'buildResult')
                     sh 'ls -ltr' 
 					sh 'ls -ltra unit-tests'
+					sh 'ls -ltra application/target/classes'
                     recordIssues enabledForFailure: true, tools: [pmdParser()]
                     junit '**/target/surefire-reports/*Test.xml' 
                     jacoco exclusionPattern: '**/*Test*.class', inclusionPattern: '**/*.class', runAlways: true                
