417b0463a614e8cbecc2dc8cac35b09171f881c5
jyotichaudhury89 <jyoti.chaudhury@capgemini.com>
2021-09-07 21:47:30 +0530

    added email notification code

--- vars/piperPipeline.groovy
+++ vars/piperPipeline.groovy
@@ -88,11 +88,6 @@ void call(parameters) {
             /* https://jenkins.io/doc/book/pipeline/syntax/#post */
             success {
 			    buildSetResult(currentBuild)
-				emailext body: """Hello,</br></br>
-                            ${env.JOB_NAME} - Build # $BUILD_NUMBER ${currentBuild.currentResult}</br></br>
-                            Please check console output <a href='$BUILD_URL'>here</a> to view the details.</br></br></br>
-                            Regards,</br>
-                            CIA4Auto DevOps Team""", subject: "Attention Required: ${env.JOB_NAME}- Jenkins Build ${currentBuild.currentResult}", to: "${emailTo}"
 			}
             aborted {buildSetResult(currentBuild, 'ABORTED')}
             failure {
