a805412cb51f9a7869dd4cf86609ca8d9717e79e
jyotichaudhury89 <jyoti.chaudhury@capgemini.com>
2021-10-03 15:04:21 +0530

    added custom code for istanbul cobertura report

--- vars/piperPipelineStageBuild.groovy
+++ vars/piperPipelineStageBuild.groovy
@@ -69,9 +69,11 @@ void call(Map parameters = [:]) {
                 testsPublishResults script: script, junit: [updateResults: true]
                 checksPublishResults script: script
             } finally {
+			    //custom changes
 			    recordIssues enabledForFailure: true, tools: [pmdParser()]
 				jacoco exclusionPattern: '**/*Test*.class', inclusionPattern: '**/*.class', runAlways: true
-				publishHTML([allowMissing: false, alwaysLinkToLastBuild: true, keepAll: true, reportDir: './coverage/', reportFiles: 'index.html', reportName: 'Coverage Report', reportTitles: 'Coverage Report'])
+				//publishHTML([allowMissing: false, alwaysLinkToLastBuild: true, keepAll: true, reportDir: './coverage/', reportFiles: 'index.html', reportName: 'Coverage Report', reportTitles: 'Coverage Report'])
+				publishCoverage adapters: [istanbulCoberturaAdapter('./coverage/cobertura-coverage.xml')], sourceFileResolver: sourceFiles('NEVER_STORE')
                 if (config.sonarExecuteScan) {
                     sonarExecuteScan script: script
                 }
